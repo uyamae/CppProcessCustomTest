4667ed7cdba517847ace4f3a02cea56792448a57
uyamae <uyamae@gmail.com>
2023-10-29 23:42:33 +0900

    add CppCustomTask project

---
+++ CppCustomTask/CppCustomTask.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.Build.Framework;
+using Microsoft.Build.Utilities;
+
+namespace CppCustomTask
+{
+    public class CppCustomTask : Task
+    {
+        public string SourcePath { get; set; }
+        public string Definitions { get; set; }
+        public string IncludePaths { get; set; }
+        public override bool Execute()
+        {
+            Log.LogMessage($"CppCustomTask");
+            Log.LogMessage($"SourcePath = {SourcePath}");
+            Log.LogMessage($"Definitions = {Definitions}");
+            Log.LogMessage($"IncludePaths = {IncludePaths}");
+            return true;
+        }
+    }
+}
